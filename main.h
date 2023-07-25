8f0aa3735a9f58001c7a605f5210661332c78724
Collins O. Birundu <cbirundu23@gmail.com>
2023-07-24 18:36:55 -0700

    My_main.h_file

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef _main_h
+#define _main_h
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * struct op - flag / function object
+ * @c: flag
+ * @f: function
+ */
+
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list);
+} op_t;
+
+int _putchar(char c);
+int _printChar(va_list values);
+int _printString(va_list values);
+int _printDec(va_list values);
+int _printInt(va_list ar_numlist);
+int (*get_func(char s))(va_list values);
+int _printf(const char *format, ...);
+#endif
